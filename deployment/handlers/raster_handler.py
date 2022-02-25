a5ea49c04267ded90760236db30077d274dfd5bd
sharkinsspatial <sharkinsgis@gmail.com>
2022-02-25 12:11:27 -0600

    Include temporary Cumulus s3credential environment settings.

--- deployment/handlers/raster_handler.py
+++ deployment/handlers/raster_handler.py
@@ -1,10 +1,15 @@
 """AWS Lambda handler."""
 
 import logging
+import os
 
 from mangum import Mangum
 
 from eoapi.raster.app import app
+
+os.environ["AWS_SECRET_ACCESS_KEY"] = os.environ["SECRET_ACCESS_KEY"]
+os.environ["AWS_ACCESS_KEY_ID"] = os.environ["ACCESS_KEY_ID"]
+os.environ["AWS_SESSION_TOKEN"] = os.environ["SESSION_TOKEN"]
 
 logging.getLogger("mangum.lifespan").setLevel(logging.ERROR)
 logging.getLogger("mangum.http").setLevel(logging.ERROR)
